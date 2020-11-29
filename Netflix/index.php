c07746aee1be26309231d6550c9883b214b37ad5
duda-arch <andre06carvalho@gmail.com>
2020-11-29 18:09:43 -0300

    Primeiro Commit

---
+++ Netflix/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Netflix copy</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="layout.css  " />
+
+
+    <link rel="stylesheet" href="owl.carousel.min.css">
+    <link rel="stylesheet" href="owl.theme.default.min.css">
+
+</head>
+<body>
+		
+			<header>
+					<img id="logo" src="imagens/logo.png">
+						<nav>
+							<a href="">Inicio</a>
+							<a href="">Séries</a>
+							<a href="">Filmes</a>
+							<a href="">Documentários</a>
+						</nav>
+			</header>
+		
+		
+ <main>
+	  <div class="principal">	
+			<div class="container">
+			  	
+			  				<h3 class="titulo">HOUSE OF CARDS</h3>
+			  				<p class="descricao"> Nada pode impedir o político sem escrúpulos Frank Underwood de conquistar Washington. Assista agora a nova temporada de House of Cards que está imperdível.</p>
+
+	
+			
+
+							<button class="botao">
+									ASSISTIR AGORA
+							</button>
+							<button class="botao">
+									MAIS INFORMAÇÕES
+							</button>
+			</div>	
+	  </div>
+</main>	
+<div class="carrosel-filmes">
+        <div class="owl-carousel owl-theme">
+                <div class="item">
+                    <img class="box-filme" src="imagens/mini1.jpg" alt="" srcset="">
+                </div>
+                <div class="item">
+                    <img class="box-filme" src="imagens/mini2.jpg" alt="" srcset="">
+                </div>
+                <div class="item">
+                    <img class="box-filme" src="imagens/mini3.jpg" alt="" srcset="">
+                </div>
+                <div class="item">
+                    <img class="box-filme" src="imagens/mini4.jpg" alt="" srcset="">
+                </div>
+                <div class="item">
+                    <img class="box-filme" src="imagens/mini5.jpg" alt="" srcset="">
+                </div>
+                <div class="item">
+                    <img class="box-filme" src="imagens/mini6.jpg" alt="" srcset="">
+                </div>
+                <div class="item">
+                    <img class="box-filme" src="imagens/mini7.jpg" alt="" srcset="">
+                </div>
+                <div class="item">
+                    <img class="box-filme" src="imagens/mini8.jpg" alt="" srcset="">
+                </div>
+                <div class="item">
+                    <img class="box-filme" src="imagens/mini9.jpg" alt="" srcset="">
+                </div>
+                <div class="item">
+                    <img class="box-filme" src="imagens/mini10.jpg" alt="" srcset="">
+                </div>
+             
+             
+        </div>
+    </div>
+
+    
+
+
+    <script src="https://kit.fontawesome.com/2c36e9b7b1.js"></script>
+    <script src="jquery.min.js"></script>
+    <script src="owl.carousel.min.js"></script>
+    <script src="setup.js"></script>
+
+</body>
+</html>
